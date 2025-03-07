aa04a40431114322753a1e4cdc458ec9afb13a75
Jithin John <jithin.john@live.com>
2025-03-07 22:10:20 +0530

    add ngrx

---
+++ src/app/books/book.actions.ts
@@ -0,0 +1,5 @@
+import { createAction, props } from "@ngrx/store";
+import { Book } from "../models/book";
+
+export const AddBook = createAction("[Book] Add Book", props<{ book: Book }>());
+export const RemoveBook = createAction("[Book] Remove Book", props<{ bookId: string }>());
